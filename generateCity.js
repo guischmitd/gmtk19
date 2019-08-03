ae9c5466d69299620952668414594ff25c9e3988
Leonardo <leoniro@gmail.com>
2019-08-03 20:47:56 -0300

    city generating really almost ok

--- generateCity.js
+++ generateCity.js
@@ -9,7 +9,7 @@ function generateCity(citySpec){
       nodesList.push = [i,j];
 
       nodeIndex = i + j*(w+1);
-      if ((i < w) && (j < h)) {
+      if ((i < w) &&   (j < h)) {
         edgeList.push = [ nodeIndex, (nodeIndex+1)];
         edgeList.push = [ nodeIndex, (nodeIndex+w+1)];
       }
@@ -20,7 +20,7 @@ function generateCity(citySpec){
         edgeList.push = [ nodeIndex, (nodeIndex+1)];
       }
 
-      tags.push = 'interesection' + nodeIndex.toString()
+      tags.push = 'intersection' + nodeIndex.toString()
     }
   }
 
@@ -37,12 +37,95 @@ function generateCity(citySpec){
       structureCoordinates.push = [getRandomInt(0,w/3),getRandomInt(0,h/3)].map( function(x) {return x*3} )
     }
     // check for duplicates
-    let structureCoordinatesUnique = structureCoordinates.filter(function(item, index) {return structure.indexOf(item) >= index;});
+    let structureCoordinatesUnique = structureCoordinates.filter(function(item, index) {return structureCoordinates.indexOf(item) >= index;});
     if (structureCoordinatesUnique.length == structureCoordinates.length) {
       duplicateEntries = false;
     }
   }
+
+  // Assign entrances and tags
+  for (let i=0; i < hospitals; i++) {
+    let coord = structureCoordinates.splice(0,1);
+    let mapCoord = coord[0];
+    let entranceCoord = mapCoord;
+
+    let randAux = getRandomInt(0,3);
+    switch (randAux) {
+      case 1:
+        entranceCoord[0] = entranceCoord[0] + 2;
+        break;
+      case 2:
+        entranceCoord[1] = entranceCoord[1] + 3;
+        break;
+      case 3:
+        entranceCoord[0] = entranceCoord[0] + 2;
+        entranceCoord[1] = entranceCoord[1] + 3;
+    }
+
+    let nodeN = nodesList.indexOf(entranceCoord)
+
+    nodesList.push = mapCoord;
+    edgeList.push = [(nodesList.length - 1) ,node N];
+    tags.push = 'hospital' + i.toString();
+  }
+
+  for (let i=0; i < schools; i++) {
+    let coord = structureCoordinates.splice(0,1);
+    let mapCoord = coord[0];
+    let entranceCoord = mapCoord;
+
+    let randAux = getRandomInt(0,3);
+    switch (randAux) {
+      case 1:
+        entranceCoord[0] = entranceCoord[0] + 2;
+        break;
+      case 2:
+        entranceCoord[1] = entranceCoord[1] + 2;
+        break;
+      case 3:
+        entranceCoord[0] = entranceCoord[0] + 2;
+        entranceCoord[1] = entranceCoord[1] + 2;
+    }
+
+    let nodeN = nodesList.indexOf(entranceCoord)
+
+    nodesList.push = mapCoord;
+    edgeList.push = [(nodesList.length - 1) , nodeN];
+    tags.push = 'school' + i.toString();
+  }
+
+  for (let i=0; i < malls; i++) {
+    let coord = structureCoordinates.splice(0,1);
+    let mapCoord = coord[0];
+    let entranceCoord = mapCoord;
+
+    let randAux = getRandomInt(0,3);
+    switch (randAux) {
+      case 1:
+        entranceCoord[0] = entranceCoord[0] + 3;
+        break;
+      case 2:
+        entranceCoord[1] = entranceCoord[1] + 3;
+        break;
+      case 3:
+        entranceCoord[0] = entranceCoord[0] + 3;
+        entranceCoord[1] = entranceCoord[1] + 3;
+    }
+
+    let nodeN = nodesList.indexOf(entranceCoord)
+
+    nodesList.push = mapCoord;
+    edgeList.push = [(nodesList.length - 1) , nodeN];
+    tags.push = 'mall' + i.toString();
+  }
+
+  // Clear edges overlapping with buildings
+
+  
+
 }
+
+
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
